3efa54fbcab993dab49e1ed5fd39e98ceb9ca35a
Petros Demetriou <dimitriupe@gmail.com>
2020-02-16 00:09:30 +0200

    2nd commit

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie';
 import SavedList from './Movies/SavedList';
-import { Route } from 'react-router-dom'
+import { BrowserRouter as Link, Route } from 'react-router-dom';
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
@@ -14,6 +14,10 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
+      <ul>
+            <li><Link to='/'>Movie List</Link></li>
+            <li><Link to='/movies/:id'>Movies</Link></li>
+      </ul>
       <Route path="/" exact component={MovieList} />
       <Route path="/movies/:id" component={Movie} />
     </div>
